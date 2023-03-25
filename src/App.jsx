5de7858aa7f018da73fb0162f6a5bb698f5160f4
swtt-pch <i.greygomes3@gmail.com>
2023-03-25 18:56:40 -0300

    cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { Flex, useMediaQuery, ChakraProvider, extendTheme } from '@chakra-ui/react'
-import './App.css'
 import Form from './components/Form'
 import SideBar from './components/Sidebar'
 import theme from './theme'
-
 
 function App() {
   const [isMobile] = useMediaQuery('(max-width: 768px)')
